e51598ade3f62c67890b9a851f0c811ecf6628dd
devmargooo <devmargooo@gmail.com>
2025-04-05 12:23:48 +0300

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from 'next';
 
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+const repo = 'page-router-example'; // имя репозитория
+
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
   reactStrictMode: true,
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 export default nextConfig;
